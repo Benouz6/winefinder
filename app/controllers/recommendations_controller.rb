d5705bdf0536b82ea0d5a07b070b8dc8f114dd58
benoit leboucher <ben.lebou@gmail.com>
2021-11-23 15:34:29 -0500

    Try to sort by price

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -1,2 +1,9 @@
 class RecommendationsController < ApplicationController
+  def index
+    @wines = Wine.all
+  end
 end
+
+
+# Client.where(
+#   "created_at > ? AND created_at < ?", params[:start_date], params[:end_date])
