574955586d160b7ff5110d62c663491b98cfe82b
Alaskar Talibov <aleskert@yahoo.fr>
2021-11-23 13:59:30 -0500

    Seed done without photo

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,55 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_11_22_215126) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "food_pairings", force: :cascade do |t|
+    t.bigint "food_id", null: false
+    t.bigint "wine_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["food_id"], name: "index_food_pairings_on_food_id"
+    t.index ["wine_id"], name: "index_food_pairings_on_wine_id"
+  end
+
+  create_table "foods", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "inventories", force: :cascade do |t|
+    t.integer "bottle_count"
+    t.bigint "store_id", null: false
+    t.bigint "wine_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["store_id"], name: "index_inventories_on_store_id"
+    t.index ["wine_id"], name: "index_inventories_on_wine_id"
+  end
+
+  create_table "stores", force: :cascade do |t|
+    t.string "adress"
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "wines", force: :cascade do |t|
+    t.string "name"
+    t.string "region"
+    t.string "description"
+    t.string "color"
+    t.integer "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "food_pairings", "foods"
+  add_foreign_key "food_pairings", "wines"
+  add_foreign_key "inventories", "stores"
+  add_foreign_key "inventories", "wines"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,84 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "open-uri"
+
+Wine.create(
+  name: "Alois Lageder",
+  color: "White",
+  region: "Trentin Haut-Adige",
+  description: "Excellent pinot blanc, a perfect wine to take in summer and as an aperitif.",
+  price: 1975
+)
+
+Wine.create(
+  name: "Bourgogne Aligoté",
+  color: "White",
+  region: "Bourgogne",
+  description: "Excellent everywhere, as an aperitif, to accompany a starter or the main course.",
+  price: 1790
+)
+
+Wine.create(
+  name: "Saumur",
+  color: "White",
+  region: "Vallée de la Loire, L'Anjou and le Saumurois",
+  description: "Gorgeous aromas and dry. Good with oysters",
+  price: 1895
+)
+
+Wine.create(
+  name: "Ambo Grigio Friuli",
+  color: "White",
+  region: "Friuli-Venezia Giulia",
+  description: "Perfect for having a nice chilled glass on a hot summer day",
+  price: 1530
+)
+
+Wine.create(
+  name: "Alsace Pinot Blanc",
+  color: "White",
+  region: "Alsace",
+  description: "It’s light and fresh tasting and great with fish",
+  price: 1565
+)
+
+Wine.create(
+  name: "Tommasi Valpolicella",
+  color: "Red",
+  region: "Veneto",
+  description: "It goes very well with pasta and shrimp without a heavy sauce",
+  price: 1565
+)
+Wine.create(
+  name: "Apothic Red",
+  color: "Red",
+  region: "Californie",
+  description: "Excellent wine with pork pasta as well as stews and red meats",
+  price: 1535
+)
+
+Wine.create(
+  name: "Duas Quintas",
+  color: "Red",
+  region: "Porto/Douro",
+  description: "Would go very well with meats on the grill.",
+  price: 1905
+)
+
+Wine.create(
+  name: "Langhe Nebbiolo",
+  color: "Red",
+  region: "Piedmont",
+  description: "A sublime pairing to game terrine or a veal chop.",
+  price: 2835
+)
+
+Wine.create(
+  name: "Zig Zag",
+  color: "Red",
+  region: "Vallée de la Loire, L'Auvergne",
+  description: "Enjoy with a chicken breast stuffed with sundried tomatoes, or even with burritos.",
+  price: 2470
+)
