47cd26ff14e15a09831237dfc5bd685d129dacf4
benoit leboucher <ben.lebou@gmail.com>
2021-11-24 16:55:48 -0500

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_22_215126) do
+ActiveRecord::Schema.define(version: 2021_11_24_213351) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2021_11_22_215126) do
     t.integer "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "saq_id"
   end
 
   add_foreign_key "food_pairings", "foods"
