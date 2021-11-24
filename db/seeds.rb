182469b854dcd81539fb51fd6bd032a3be8460b6
benoit leboucher <ben.lebou@gmail.com>
2021-11-24 16:51:41 -0500

    Seeds created

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,118 +11,35 @@ def fetch_description(link)
 end
 
 page = 1
-colors = ["red", "white"]
+colors = ["red", "white"] * 4
 
-1.times do
-  url = "https://www.saq.com/en/products/wine/#{colors.first}-wine?p=#{page}"
+colors.each do |color|
+  url = "https://www.saq.com/en/products/wine/#{color}-wine?p=#{page}"
   html_file = URI.open(url).read
   html_doc = Nokogiri::HTML(html_file)
 
   html_doc.search(".product-item-info").each do |element|
-    pp name = element.search(".product-item-name").text.strip
+    name = element.search(".product-item-name").text.strip.gsub(/\s+/, " ")
     price_float = element.search(".price")[0].text.strip.gsub(/[[:space:]]\$/, "").gsub(",", "").gsub("$", "").to_f
-    pp price = (price_float*100).to_i
-    pp saq_id = element.search("[data-product-id]").attribute("data-product-id").value
+    price = (price_float*100).to_i
+    saq_id = element.search("[data-product-id]").attribute("data-product-id").value
     temp_origin = element.search(".product-item-identity-format span").text.strip.gsub(/[|]/, "").split(/\b/)
-    pp origin = temp_origin.reject(&:blank?).last
+    origin = temp_origin.reject(&:blank?).last
 
     link = element.search(".product-item-link").attribute('href').value
-    pp description = fetch_description(link)
-    color = colors
+    description = fetch_description(link)
 
     Wine.create!(
       name: name,
       price: price,
-      origin: origin,
-      color: color,
       saq_id: saq_id,
-      grape: description
+      region: origin,
+      color: color,
+      description: description
     )
   end
   page += 50
 end
-
-
-
-
-# Wine.create(
-#   name: "Alois Lageder",
-#   color: "White",
-#   region: "Trentin Haut-Adige",
-#   description: "Excellent pinot blanc, a perfect wine to take in summer and as an aperitif.",
-#   price: 19
-# )
-
-# Wine.create(
-#   name: "Bourgogne Aligoté",
-#   color: "White",
-#   region: "Bourgogne",
-#   description: "Excellent everywhere, as an aperitif, to accompany a starter or the main course.",
-#   price: 17
-# )
-
-# Wine.create(
-#   name: "Saumur",
-#   color: "White",
-#   region: "Vallée de la Loire, L'Anjou and le Saumurois",
-#   description: "Gorgeous aromas and dry. Good with oysters",
-#   price: 18
-# )
-
-# Wine.create(
-#   name: "Ambo Grigio Friuli",
-#   color: "White",
-#   region: "Friuli-Venezia Giulia",
-#   description: "Perfect for having a nice chilled glass on a hot summer day",
-#   price: 12
-# )
-
-# Wine.create(
-#   name: "Alsace Pinot Blanc",
-#   color: "White",
-#   region: "Alsace",
-#   description: "It’s light and fresh tasting and great with fish",
-#   price: 11
-# )
-
-# Wine.create(
-#   name: "Tommasi Valpolicella",
-#   color: "Red",
-#   region: "Veneto",
-#   description: "It goes very well with pasta and shrimp without a heavy sauce",
-#   price: 21
-# )
-# Wine.create(
-#   name: "Apothic Red",
-#   color: "Red",
-#   region: "Californie",
-#   description: "Excellent wine with pork pasta as well as stews and red meats",
-#   price: 27
-# )
-
-# Wine.create(
-#   name: "Duas Quintas",
-#   color: "Red",
-#   region: "Porto/Douro",
-#   description: "Would go very well with meats on the grill.",
-#   price: 10
-# )
-
-# Wine.create(
-#   name: "Langhe Nebbiolo",
-#   color: "Red",
-#   region: "Piedmont",
-#   description: "A sublime pairing to game terrine or a veal chop.",
-#   price: 18
-# )
-
-# Wine.create(
-#   name: "Zig Zag",
-#   color: "Red",
-#   region: "Vallée de la Loire, L'Auvergne",
-#   description: "Enjoy with a chicken breast stuffed with sundried tomatoes, or even with burritos.",
-#   price: 22
-# )
 
 Food.create(
   name: "Chicken"
@@ -135,3 +52,5 @@ Food.create(
 Food.create(
   name: "Meat"
 )
+
+puts "Seed created"
