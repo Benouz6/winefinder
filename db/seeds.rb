b4240e85694697e14680e9c62b0c2c3b79918da7
benoit leboucher <ben.lebou@gmail.com>
2021-11-24 16:05:48 -0500

    Seeds almost ready

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,12 @@ require "open-uri"
 Wine.destroy_all
 Food.destroy_all
 
+def fetch_description(link)
+  html_file = URI.open(link).read
+  html_doc = Nokogiri::HTML(html_file)
+  html_doc.search("[data-th]")[3].text.strip
+end
+
 page = 1
 colors = ["red", "white"]
 
@@ -12,28 +18,31 @@ colors = ["red", "white"]
   html_file = URI.open(url).read
   html_doc = Nokogiri::HTML(html_file)
 
-  html_doc.search(".product-item-info").first(1).each do |element|
-    # pp element
-    name = element.search(".product-item-name").text.strip
-    price = element.search(".price")[0].text.strip.gsub(/[[:space:]]\$/, "").gsub(",", "").to_i
-    saq_id = element.search("[data-product-id]").attribute("data-product-id").value
+  html_doc.search(".product-item-info").each do |element|
+    pp name = element.search(".product-item-name").text.strip
+    price_float = element.search(".price")[0].text.strip.gsub(/[[:space:]]\$/, "").gsub(",", "").gsub("$", "").to_f
+    pp price = (price_float*100).to_i
+    pp saq_id = element.search("[data-product-id]").attribute("data-product-id").value
     temp_origin = element.search(".product-item-identity-format span").text.strip.gsub(/[|]/, "").split(/\b/)
-    origin = temp_origin.reject(&:blank?).last
+    pp origin = temp_origin.reject(&:blank?).last
 
-    p element.search(".product-item-link").attribute('href').value
+    link = element.search(".product-item-link").attribute('href').value
+    pp description = fetch_description(link)
+    color = colors
 
-    # price = price-final_price
+    Wine.create!(
+      name: name,
+      price: price,
+      origin: origin,
+      color: color,
+      saq_id: saq_id,
+      grape: description
+    )
   end
-  #  page += 50
+  page += 50
 end
 
-# Wine.create!(
-#   name: name,
-#   price: price,
-#   origin: origin,
-#   color: color,
-#   saq_id: saq_id
-# )
+
 
 
 # Wine.create(
