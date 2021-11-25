7cd35fec2fcb2fc2b7ea7aa655dee6affd99719b
benoit leboucher <ben.lebou@gmail.com>
2021-11-25 12:04:18 -0500

    Try to display

--- app/controllers/recommendations_controller.rb
+++ app/controllers/recommendations_controller.rb
@@ -1,11 +1,13 @@
 class RecommendationsController < ApplicationController
   def index
-    if params[:color].present?
-      @wines = Wine.where("color = ?", params[:color]) &&
-      params[:price].present?
-      @wines = Wine.where("price <= ?", params[:price].to_i)
-    else
-      @wines = Wine.all
-    end
+    @wines = Wine.first(10)
+    @foods = Food.all
+    # if params[:color].present?
+    #   @wines = Wine.where("color = ?", params[:color]) &&
+    #   params[:price].present?
+    #   @wines = Wine.where("price <= ?", params[:price].to_i)
+    # else
+    #   @wines = Wine.all
+    # end
   end
 end
