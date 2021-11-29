79a65957844374c311ad3d492163e42506bab5c7
umberto najar <najar.umberto@gmail.com>
2021-11-29 14:06:37 -0500

    created article method, route and view

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'pages/search', to: 'pages#search'
   get '/map', to: 'recommendations#map'
+  get 'pages/article', to: 'pages#article'
 
   resources :recommendations, only: [:index, :show]
 end
