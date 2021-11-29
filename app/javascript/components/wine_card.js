e7518ba94fd567ff8e86160eb5cba69ec6dd3fa0
felix leveille <fil.leveille@hotmail.com>
2021-11-29 17:48:40 -0500

    refactored code

--- app/javascript/components/wine_card.js
+++ app/javascript/components/wine_card.js
@@ -10,12 +10,12 @@ const small_to_large = (small_cards, large_cards) => {
   small_cards.forEach((small_card, index) => {
     small_card.addEventListener('click', (event) => {
       // hide small card
-      event.currentTarget.classList.add('d-none')
-      event.currentTarget.classList.remove('d-flex')
+      event.currentTarget.classList.add('disabled-card')
+      event.currentTarget.classList.remove('active-card')
 
       // show large card
-      large_cards[index].classList.add('d-flex')
-      large_cards[index].classList.remove('d-none')
+      large_cards[index].classList.add('active-card')
+      large_cards[index].classList.remove('disabled-card')
     });
   });
 }
@@ -24,12 +24,12 @@ const large_to_small = (large_cards, small_cards) => {
   large_cards.forEach((large_card, index) => {
     large_card.addEventListener('click', (event) => {
       // hide large card
-      event.currentTarget.classList.add('d-none')
-      event.currentTarget.classList.remove('d-flex')
+      event.currentTarget.classList.add('disabled-card')
+      event.currentTarget.classList.remove('active-card')
 
       // show small card
-      small_cards[index].classList.add('d-flex')
-      small_cards[index].classList.remove('d-none')
+      small_cards[index].classList.add('active-card')
+      small_cards[index].classList.remove('disabled-card')
     });
   });
 }
