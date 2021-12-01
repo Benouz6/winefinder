bdfcb36b392b56f343741e07984b1ae8a7b72bc4
felix leveille <fil.leveille@hotmail.com>
2021-12-01 17:41:05 -0500

    Finished javascript

--- app/javascript/components/form_button.js
+++ app/javascript/components/form_button.js
@@ -3,12 +3,12 @@ const toggleBtnState = () => {
 
   if (button) {
     button.addEventListener('click', (event) => {
-      console.log(event.currentTarget);
       event.currentTarget.style.opacity = '85%';
       event.currentTarget.innerHTML = '<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Finding Wines';
 
-      sleep(250);
-      event.currentTarget.disabled = true;
+      // setTimeout(function () {
+      //   event.currentTarget.disabled = true;
+      // }, 2000);
     });
   }
 }
