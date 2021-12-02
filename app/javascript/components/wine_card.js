119bdd8a30454c0926d1b5c6f8d7d82e90f4c557
felix leveille <fil.leveille@hotmail.com>
2021-12-02 11:46:55 -0500

    added intermediate card

--- app/javascript/components/wine_card.js
+++ app/javascript/components/wine_card.js
@@ -1,6 +1,7 @@
 const toggleCardState = () => {
   const small_cards = Array.from(document.getElementsByClassName('wine-card-small'));
   const large_cards = Array.from(document.getElementsByClassName('wine-card-large'));
+  const intermediate_cards = Array.from(document.getElementsByClassName('wine_card_intermediate'));
 
   small_to_large(small_cards, large_cards);
   large_to_small(large_cards, small_cards);
@@ -10,12 +11,21 @@ const small_to_large = (small_cards, large_cards) => {
   small_cards.forEach((small_card, index) => {
     small_card.addEventListener('click', (event) => {
       // hide small card
-      event.currentTarget.classList.add('disabled-card')
-      event.currentTarget.classList.remove('active-card')
+      event.currentTarget.classList.add("card-fade-out");
+
+
+
+      sleep(2000);
+
+
+
+
+      event.currentTarget.classList.add('disabled-card');
+      event.currentTarget.classList.remove('active-card');
 
       // show large card
-      large_cards[index].classList.add('active-card')
-      large_cards[index].classList.remove('disabled-card')
+      large_cards[index].classList.add('active-card');
+      large_cards[index].classList.remove('disabled-card');
     });
   });
 }
@@ -24,12 +34,12 @@ const large_to_small = (large_cards, small_cards) => {
   large_cards.forEach((large_card, index) => {
     large_card.addEventListener('click', (event) => {
       // hide large card
-      event.currentTarget.classList.add('disabled-card')
-      event.currentTarget.classList.remove('active-card')
+      event.currentTarget.classList.add('disabled-card');
+      event.currentTarget.classList.remove('active-card');
 
       // show small card
-      small_cards[index].classList.add('active-card')
-      small_cards[index].classList.remove('disabled-card')
+      small_cards[index].classList.add('active-card');
+      small_cards[index].classList.remove('disabled-card');
     });
   });
 }
--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -1,7 +1,6 @@
 import places from 'places.js';
 
 const initAutocomplete = () => {
-  console.log("test")
   const addressInput = document.querySelector('#bottle_search');
   if (addressInput) {
     places({ container: addressInput});
