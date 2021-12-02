8c63a30ff5c64f9310a99274f81d25a34f81da59
felix leveille <fil.leveille@hotmail.com>
2021-12-02 13:52:10 -0500

    Dropdown transition on large card added

--- app/javascript/components/wine_card.js
+++ app/javascript/components/wine_card.js
@@ -1,17 +1,39 @@
 const toggleCardState = () => {
   const small_cards = Array.from(document.getElementsByClassName('wine-card-small'));
   const large_cards = Array.from(document.getElementsByClassName('wine-card-large'));
-  const intermediate_cards = Array.from(document.getElementsByClassName('wine_card_intermediate'));
+  const intermediate_cards = Array.from(document.getElementsByClassName('wine-card-intermediate'));
 
-  small_to_large(small_cards, large_cards);
-  large_to_small(large_cards, small_cards);
+  small_to_large(small_cards, large_cards, intermediate_cards);
+  large_to_small(large_cards, small_cards, intermediate_cards);
 }
 
-const small_to_large = (small_cards, large_cards) => {
+const small_to_large = (small_cards, large_cards, intermediate_cards) => {
   small_cards.forEach((small_card, index) => {
     small_card.addEventListener('click', (event) => {
       // hide small card
+
+      // fade out small card
       event.currentTarget.classList.add("card-fade-out");
+
+      event.currentTarget.classList.add('disabled-card');
+      event.currentTarget.classList.remove('active-card');
+      event.currentTarget.classList.remove("card-fade-out");
+
+
+      intermediate_cards[index].classList.remove('wine-card-intermediate-small')
+      intermediate_cards[index].classList.add('wine-card-intermediate-full')
+
+
+
+      setTimeout(function () {
+      }, 200);
+
+      // pop in large card in it's small state
+      large_cards[index].classList.add('active-card');
+      large_cards[index].classList.remove('disabled-card');
+
+
+
 
 
 
@@ -20,12 +42,8 @@ const small_to_large = (small_cards, large_cards) => {
 
 
 
-      event.currentTarget.classList.add('disabled-card');
-      event.currentTarget.classList.remove('active-card');
 
       // show large card
-      large_cards[index].classList.add('active-card');
-      large_cards[index].classList.remove('disabled-card');
     });
   });
 }
