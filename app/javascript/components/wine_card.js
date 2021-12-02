11e8f0abfaf33211bd1ca3d6d1b75dec6723ed05
felix leveille <fil.leveille@hotmail.com>
2021-12-02 16:40:52 -0500

    added fade in and out functionalities

--- app/javascript/components/wine_card.js
+++ app/javascript/components/wine_card.js
@@ -3,41 +3,67 @@ const toggleCardState = () => {
   const large_cards = Array.from(document.getElementsByClassName('wine-card-large'));
   const intermediate_cards = Array.from(document.getElementsByClassName('wine-card-intermediate'));
 
+  // small_cards.forEach =
+
   small_to_large(small_cards, large_cards, intermediate_cards);
   large_to_small(large_cards, small_cards, intermediate_cards);
+
+  large_cards.forEach((large_card) => {
+    console.log(large_card.clientHeight);
+  });
 }
 
 const small_to_large = (small_cards, large_cards, intermediate_cards) => {
   small_cards.forEach((small_card, index) => {
     small_card.addEventListener('click', (event) => {
-      // hide small card
-
-      // fade out small card
-      event.currentTarget.classList.add("card-fade-out");
-
-      event.currentTarget.classList.add('disabled-card');
-      event.currentTarget.classList.remove('active-card');
-      event.currentTarget.classList.remove("card-fade-out");
+      let intermediate_card = intermediate_cards[index];
+      let large_card = large_cards[index];
+      let small_card = event.currentTarget;
 
 
-      intermediate_cards[index].classList.remove('wine-card-intermediate-small')
-      intermediate_cards[index].classList.add('wine-card-intermediate-full')
+      // pop in large card in it's small invisible state
+
+      intermediate_card.classList.remove('disabled-card');
+      intermediate_card.classList.add('active-card');
+
+
+      // fade out small card and fade in large card
+      intermediate_card.classList.add('fade-in');
+      small_card.classList.add('fade-out')
 
 
 
+      // event.currentTarget.id = "card-fade-out";
+      // setTimeout(function () {
+      //   // intermediate_card.classList.remove('card-invisible');
+      //   invermediate_card.id = '';
+      // }, 1000);
+
+      // intermediate_card.classList.add('card-fade-in')
+
+
+
+      // intermediate_card.classList.add('card-invisible');
+
+
+      // hide small card and remove absolute position from large card
       setTimeout(function () {
-      }, 200);
+        small_card.add('disabled-card');
+        small_card.remove('active-card');
+        small_card.remove("card-fade-out");
+        intermediate_card.classList.remove('wine-card-intermediate-fixed-position')
+      }, 5000);
 
-      // pop in large card in it's small state
-      large_cards[index].classList.add('active-card');
-      large_cards[index].classList.remove('disabled-card');
+      // grow large card
+      // intermediate_card.classList.remove('wine-card-intermediate-small')
+      // intermediate_card.classList.add('wine-card-intermediate-full')
 
 
 
+      // large_cards[index].classList.add('active-card');
 
 
 
-      sleep(2000);
 
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  
+
   get 'pages/search', to: 'pages#search'
   get 'pages/article', to: 'pages#article'
-  
+
+  get 'recommendations/test', to: 'recommendations#test', as: :test
   get 'recommendations/:id/map', to: 'recommendations#map', as: :map
 
   resources :recommendations, only: [:index, :show]
