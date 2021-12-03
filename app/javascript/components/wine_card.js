45cea3991037b63e8011a7d645cc22200882f1f3
felix leveille <fil.leveille@hotmail.com>
2021-12-03 00:31:43 -0500

    finished card transitions

--- app/javascript/components/wine_card.js
+++ app/javascript/components/wine_card.js
@@ -1,114 +1,78 @@
 const toggleCardState = () => {
   const small_cards = Array.from(document.getElementsByClassName('wine-card-small'));
-  const large_cards = Array.from(document.getElementsByClassName('wine-card-large'));
-  const intermediate_cards = Array.from(document.getElementsByClassName('wine-card-intermediate'));
+  const large_cards = Array.from(document.getElementsByClassName('wine-card-intermediate'));
 
-  // small_cards.forEach =
-
-  small_to_large(small_cards, large_cards, intermediate_cards);
-  large_to_small(large_cards, small_cards, intermediate_cards);
-
-  // large_cards.forEach((large_card) => {
-  //   console.log(large_card.scrollHeight)
-  // });
+  small_to_large(small_cards, large_cards);
+  large_to_small(large_cards, small_cards);
 }
 
-const small_to_large = (small_cards, large_cards, intermediate_cards) => {
+const small_to_large = (small_cards, large_cards) => {
   small_cards.forEach((small_card, index) => {
     small_card.addEventListener('click', (event) => {
-      const intermediate_card = intermediate_cards[index];
+      const large_card = large_cards[index];
       const small_card = event.currentTarget;
 
       // activate large card
-      intermediate_card.classList.remove('disabled-card');
-      intermediate_card.classList.add('active-card');
+      large_card.classList.remove('disabled-card');
+      large_card.classList.add('active-card');
 
 
       // fade out small card and fade in large card
-      intermediate_card.classList.add('fade-in');
+      large_card.classList.add('fade-in');
       small_card.classList.add('fade-out');
 
       // disable small card and remove absolute position from large one
       setTimeout(function () {
-        intermediate_card.classList.remove('fade-in');
+        large_card.classList.remove('fade-in');
         small_card.classList.remove('fade-out');
 
         small_card.classList.add('disabled-card');
         small_card.classList.remove('active-card');
-        intermediate_card.classList.remove('wine-card-intermediate-fixed-position');
+        large_card.classList.remove('wine-card-intermediate-fixed-position');
 
         // grow large card
-        intermediate_card.classList.remove('wine-card-intermediate-small')
-        intermediate_card.classList.add('wine-card-intermediate-full')
+        large_card.classList.remove('wine-card-intermediate-small')
+        large_card.classList.add('wine-card-intermediate-full')
         setTimeout(function () {
-          intermediate_card.classList.remove('wine-card-intermediate-full')
+          large_card.classList.remove('wine-card-intermediate-full')
         }, 400);
       }, 350);
-
-      // use actual card height
-      // intermediate_card.style.height = `${large_card.clientHeight}px`;
-      // intermediate_card.style.setProperty('heigth', `${large_card.clientHeight}px`);
-      // large_cards[index].classList.add('active-card');
     });
   });
 }
 
-const large_to_small = (large_cards, small_cards, intermediate_cards) => {
+const large_to_small = (large_cards, small_cards) => {
   large_cards.forEach((large_card, index) => {
     large_card.addEventListener('click', (event) => {
-      const intermediate_card = intermediate_cards[index];
       const large_card = event.currentTarget;
       const small_card = small_cards[index]
 
       // shrink large card
-      intermediate_card.classList.add('wine-card-intermediate-full')
+      large_card.classList.add('wine-card-intermediate-full')
       setTimeout(function () {
-        intermediate_card.classList.remove('wine-card-intermediate-full')
-        intermediate_card.classList.add('wine-card-intermediate-small')
+        large_card.classList.remove('wine-card-intermediate-full')
+        large_card.classList.add('wine-card-intermediate-small')
 
         setTimeout(function () {
           // enable small card and add absolute position to large one
-          intermediate_card.classList.add('wine-card-intermediate-fixed-position');
+          large_card.classList.add('wine-card-intermediate-fixed-position');
+          small_card.classList.add('active-card');
+          small_card.classList.remove('disabled-card');
 
+          // fade in small card and fade out large card
+          small_card.classList.add('fade-in');
+          large_card.classList.add('fade-out');
+
+          setTimeout(function () {
+            // disable large card and clean up classes
+            small_card.classList.remove('fade-in');
+            large_card.classList.remove('fade-out');
+
+            large_card.classList.remove('active-card');
+            large_card.classList.add('disabled-card');
+          }, 350);
         }, 400);
-
-
-
-
-
-
-
-
-
-
       }, 1);
-
-
-
-
-      // fade in small card and fade out large card
-
-
-
-      // disable large card
-
-
-
-
-
-
-
-
-
-
-
-      // // hide large card
-      // event.currentTarget.classList.add('disabled-card');
-      // event.currentTarget.classList.remove('active-card');
-
-      // // show small card
-      // small_cards[index].classList.add('active-card');
-      // small_cards[index].classList.remove('disabled-card');
     });
   });
 }
